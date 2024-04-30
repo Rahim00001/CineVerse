1a8d710ae948332c89e4468a2ff51096602c26e8
Rahim00001 <rahimlpt2022@gmail.com>
2024-04-30 21:15:27 +0600

    simple footer added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="flex justify-center">
+            <p className="mt-8 mb-1 text-sm font-thin">Copyright © 2024 KamrulIslamRahim | All rights reserved</p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../Footer/Footer";
 import Movies from "../Movies/Movies";
 import ScrollToTop from "../ScrollToTop/ScrollToTop";
 
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Movies></Movies>
             <ScrollToTop></ScrollToTop>
+            <Footer></Footer>
         </div>
     );
 };
